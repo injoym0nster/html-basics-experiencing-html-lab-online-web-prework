7b6279a0c29eb8f30a25ea6073696cacb4fa91c9
Sahduddin ahmed <53123532+injoym0nster@users.noreply.github.com>
2019-08-10 11:31:39 -0500

    Update and rename html_spec.rb to done.rb

---
+++ spec/done.rb
@@ -0,0 +1,17 @@
+<h1>Domesticated Animals</h1>
+<p>FAVE ANIMALS!</p>
+<ul>
+  <li>dog</li>
+  <li>cat</li>
+  <li>horse</li>
+  <li>cat</li>
+</ul>
+st.txt@@ -0,0 +1,8 @@
+dog
+cat
+bird
+cow
+fox
+pig
+lamb
+horse
